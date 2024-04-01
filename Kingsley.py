6dbf6781bd90c535888423bb6c646b81b83dd884
Okedigwe <152255378+Okedigwe@users.noreply.github.com>
2024-04-01 20:06:35 +0100

    Add files via upload

---
+++ Kingsley.py
@@ -0,0 +1,22 @@
+print(60 + 30)
+print(98 - 43)
+print(225 * 12)
+print(80 / 20)
+print(26 / 13 * 8 - 4 + 7 **2)
+
+# Define weight and initial height
+weight = 75
+height = 5 # Initial height
+
+# Calculate BMI for different heights
+print("a. Height =", 6)
+bmi_a = weight / (6**2) # Calculate BMI for height 6
+print("BMI:", bmi_a)
+
+print("b. Height =", 7)
+bmi_b = weight / (7**2) # Calculate BMI for height 7
+print("BMI:", bmi_b)
+
+print("c. Height =", 4)
+bmi_c = weight / (4**2) # Calculate BMI for height 4
+print("BMI:", bmi_c)
